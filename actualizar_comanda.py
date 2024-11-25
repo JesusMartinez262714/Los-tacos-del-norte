0d33b294d093011f8a9b70e6193f3502f78142a9
JesusMartinez262714 <martinez.jesusc14@gmail.com>
2024-11-25 09:50:04 -0700

    eliminar productos

--- actualizar_comanda.py
+++ actualizar_comanda.py
@@ -4,35 +4,35 @@ Objetivo: Actualización de productos en una comanda en el sistema.
 """
 import utilerias as u
 
+
 # Función para verificar si existe una comanda abierta para la mesa proporcionada
-def verificar_comanda(comandas: dict, platillos: tuple):
+def verificar_comanda(comandas:dict, platillos:tuple):
     """
     Verifica si una comanda está abierta para la mesa especificada.
 
     Parámetros:
-    - comandas (dict): Diccionario con las comandas abiertas.
-    - platillos (tuple): Tupla que contiene los platillos disponibles.
+    - comandas dict: Diccionario con las comandas abiertas.
+    - platillos (Tuple[Tuple[int, str, float], ...]): Tupla que contiene los platillos disponibles.
 
-    Llama a la función para mostrar las comandas abiertas y luego solicita el número de la mesa donde se desea hacer la actualización.
-    Si la mesa tiene una comanda abierta y no ha sido pagada, muestra el resumen de la comanda y el menú de actualizaciones.
+    Retorna:
+    - Union[int, None]: El folio de la comanda si se encuentra, o None si no existe una comanda abierta para la mesa.
     """
-    u.comandas_abiertas(comandas)  # Llama a la función para verificar las comandas abiertas
-    verificar = u.validar_numerico("Ingrese el número de la mesa donde se desea hacer la actualización: ")  # Solicita el número de mesa
+    u.comandas_abiertas(comandas)
+    verificar = u.validar_numerico("Ingrese el número de la mesa donde se desea hacer la actualización: ")
     folio = u.obtener_folio_por_mesa(comandas, verificar)
-    for datos in comandas.values():  # Itera sobre las comandas abiertas
-        if verificar == datos['mesa'] and datos['estado'] == "No pagada":  # Verifica que la mesa esté abierta y no pagada
-            u.mostrar_resumen(comandas, folio)  # Muestra el resumen de la comanda
-            menu_actualizaciones(platillos, verificar, comandas)  # Muestra el menú de actualizaciones
+    for datos in comandas.values():
+        if verificar == datos['mesa'] and datos['estado'] == "No pagada":
+            u.mostrar_resumen(comandas, folio)
+            menu_actualizaciones(platillos, verificar, comandas)
             break
     else:
-        print("No hay ninguna comanda abierta para esta mesa. Intente de nuevo.")  # Si no se encuentra la mesa
-        return  
-
-
+        print("No hay ninguna comanda abierta para esta mesa. Intente de nuevo.")
+        return
+    return folio
 
 
 # Función para agregar un producto a la comanda
-def agregar_producto(platillos, verificar, comandas):
+def agregar_producto(platillos, verificar, comandas:dict):
     """
     Agrega un producto a una comanda existente.
 
@@ -69,19 +69,21 @@ def agregar_producto(platillos, verificar, comandas):
             else:
                 print("La cantidad debe ser mayor a 0. Intente de nuevo.")
 
-        for datos in comandas.values():  # Itera sobre las comandas abiertas
+        # Cálculo del subtotal basado en el platillo seleccionado
+        subtotal=cantidad*platillos[platillo-1][2]
+
+        for comanda in comandas.values():  # Itera sobre las comandas abiertas
+            
             folio = u.obtener_folio_por_mesa(comandas, verificar)  # Obtiene el folio de la coma
             comanda = comandas.get(folio)  # Obtiene la comanda por folio
-
+            datos=comanda
             if comanda:  # Verifica si la comanda existe
                 platillo_encontrado = False
                 # Busca si el platillo ya está en la comanda
-                for i in range(len(datos["platillos"])):
-                    # Cálculo del subtotal basado en el platillo seleccionado
-                    subtotal = cantidad * platillos[0][2] if platillo == 1 else cantidad * platillos[1][2] if platillo == 2 else cantidad * platillos[2][2] if platillo == 3 else cantidad* platillos[3][2] if platillo == 4 else cantidad * platillos[4][2] if platillo == 5 else cantidad * platillos[5][2] if platillo == 6 else cantidad * platillos[6][2] if platillo == 7 else cantidad * platillos[7][2] if platillo == 8 else cantidad * platillos[8][2] if platillo == 9 else cantidad * platillos[9][2] if platillo == 10 else 0
-                    
+                for i in range(len(datos["platillos"])): #
+    
                     platillo = u.validar_mismo_platillo(platillo)  # Valida si el platillo existe en el sistema
-
+                    
                     if platillo == datos["platillos"][i][0]:  # Si se encuentra el platillo
                         # Si el platillo ya existe, se actualiza su cantidad y subtotal
                         datos["platillos"][i] = (datos["platillos"][i][0], datos["platillos"][i][1] + cantidad, datos["platillos"][i][2] + subtotal)
@@ -108,50 +110,91 @@ def agregar_producto(platillos, verificar, comandas):
         verificar_comanda(comandas,platillos)  # Finaliza la función de agregar producto
 
 
-def eliminar_producto():
+# Función para eliminar un producto de la comanda
+def eliminar_producto(platillos:tuple,verificar):
     """
-    Función para eliminar un producto de la comanda.
+    Elimina un producto de la comanda seleccionada.
 
+    Parámetros:
+    - platillos (Tuple[Tuple[int, str, float], ...]): Tupla con los platillos disponibles.
+
+    Actualiza el contenido de la comanda eliminando productos o cantidades según las indicaciones del usuario.
+    """
+    folio = u.obtener_folio_por_mesa(comandas, verificar)
     
-    """
-    print("Eliminar producto")
+    if folio in comandas:
+        print(f"\n{"productos de la comanda ":->25}{folio}:")
+        contador = 1
+        for platillo in comandas[folio]['platillos']:
+            print(f"{contador}. {platillo[0]} ({platillo[1]})")
+            contador += 1
+        
+        while True:
+            producto = u.validar_numerico("Ingrese el número del producto que desea eliminar: ")
+            if 1 <= producto <= len(comandas[folio]['platillos']):
+                cant_producto = comandas[folio]['platillos'][producto - 1][1]
+                nombre_producto = comandas[folio]['platillos'][producto - 1][0]
+                id_producto=u.id_por_nombre_platillo(platillos,nombre_producto)
 
+                print(f"Producto: {nombre_producto}")
+                print(f"Cantidad disponible: {cant_producto}")
 
-def aumentar_cantidad():
-    """
-    Función placeholder para aumentar la cantidad de un producto en la comanda.
-
+                cantidad = input(f"Ingrese cuántos {nombre_producto} desea eliminar (o 'todo'): ")
+                if cantidad == 'todo':
+                    del comandas[folio]['platillos'][producto - 1]
+                    print(f"{nombre_producto} ha sido completamente eliminado.")
+                elif cantidad.isdigit():
+                    cantidad = int(cantidad)
+                    if cantidad > cant_producto:
+                        print(f"Solo existen {cant_producto} de este producto.")
+                    else:
+                        continuar = input(f"¿Está seguro de que desea eliminar {cantidad} de {nombre_producto}? (s/n): ").lower()
+                        if continuar == 's':
+                            nuevo_cant = cant_producto - cantidad
+                            if nuevo_cant > 0:
+                                comandas[folio]['platillos'][producto - 1] = (nombre_producto, nuevo_cant, nuevo_cant * platillos[id_producto - 1][2])
+                                print(f"{cantidad} de {nombre_producto} ha sido eliminado.")
+                            else:
+                                del comandas[folio]['platillos'][producto - 1]
+                                print(f"{nombre_producto} ha sido completamente eliminado.")
+                else:
+                    print("Entrada no válida. Ingrese un número.")
+                
+                u.calcular_total(folio, comandas)
+                u.mostrar_resumen(comandas, folio)
+                return
+            else:
+                print("Opción no válida. Seleccione un producto de la lista.")
    
-    """
-    print("Aumentar cantidad de producto")
+   
 
 # Función que muestra el menú de actualizaciones
-def menu_actualizaciones(platillos, verificar, comandas):
+def menu_actualizaciones(platillos:tuple, verificar: int, comandas:dict) -> None:
     """
     Muestra el menú con las opciones para actualizar la comanda.
 
     Parámetros:
-    - platillos (tuple): Tupla con los platillos disponibles.
+    - platillos Tuple: Tupla con los platillos disponibles.
     - verificar (int): Número de la mesa de la comanda a actualizar.
-    - comandas (dict): Diccionario con las comandas abiertas.
+    - comandas dict: Diccionario con las comandas abiertas.
 
-    Permite al usuario elegir entre agregar un producto, eliminar uno o aumentar la cantidad de un producto.
+    Permite elegir entre agregar, eliminar o aumentar la cantidad de un producto.
     """
     print("¿Qué actualización desea realizar? ")
     print("1.- Agregar producto")
     print("2.- Eliminar producto")
     print("3.- Aumentar cantidad de producto")
     print("4.- Regresar al menú principal")
-    opcion = u.validar_numerico('Ingrese una opción: ')  # Solicita la opción de actualización
+    opcion = u.validar_numerico('Ingrese una opción: ')
     
-    if opcion == 4:  # Si la opción es 4, regresa al menú principal
+    if opcion == 4:
         return
-    elif opcion == 1:  # Si la opción es 1, agrega un producto
+    elif opcion == 1:
         agregar_producto(platillos, verificar, comandas)
-    elif opcion == 2:  # Si la opción es 2, elimina un producto
-        eliminar_producto()
-    elif opcion == 3:  # Si la opción es 3, aumenta la cantidad de un producto
-        aumentar_cantidad()
+    elif opcion == 2:
+        eliminar_producto(platillos,verificar)
+    elif opcion == 3:
+        print("Función no implementada aún.")
 
 # Bloque principal de ejecución
 if __name__ == "__main__":
@@ -178,9 +221,10 @@ if __name__ == "__main__":
             "empleado": "María López",
             "platillos": [
                 ("Tacos de Asada", 3, 60.00),  # (Nombre del platillo, Cantidad, Subtotal)
-                ("Refresco", 2, 30.00)
+                ("Refresco", 2, 30.00),
+                ("Torta de Asada", 4, 180.00)
             ],
-            "total": 90.00,
+            "total": 270.00,
             "propina": 0,
             "estado": "No pagada"  # Pueden ser pagadas o no pagadas
         },
@@ -189,7 +233,7 @@ if __name__ == "__main__":
             "cliente": "Juan Pérez",
             "empleado": "María López",
             "platillos": [
-                ("Tacos de Asada", 3, 60.00),  # (Nombre del platillo, Cantidad, Subtotal)
+                ("Torta de Asada", 3, 60.00),  # (Nombre del platillo, Cantidad, Subtotal)
                 ("Refresco", 2, 30.00)
             ],
             "total": 90.00,
@@ -213,4 +257,4 @@ if __name__ == "__main__":
     )
 
     # Simulamos el proceso de actualización de una comanda
-    verificar_comanda(comandas, platillos)
+    verificar_comanda(comandas,platillos)
