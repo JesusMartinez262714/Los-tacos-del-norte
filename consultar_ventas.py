a97cb12ba33052727e34364818ff7963ba23979f
Ramsecs <ramsesn1506@gmail.com>
2024-11-27 22:16:31 -0700

    Verificacion

--- consultar_ventas.py
+++ consultar_ventas.py
@@ -31,8 +31,6 @@ def consultar_ventas(comandas,empleados):
                     print(f"{folio }{datos['mesa']:<9}{datos['cliente']:<16}{datos['empleado']:<17}{datos['total']}")
                 else:
                     print(f"No hay ventas registradas para el empleado {Empleado}")
-        else:
-            print("no hay ventas del dia")
     return
 
 
